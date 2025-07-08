5da6f039cc1886bc70424c29e502960daca0b9b6
kgarth <kade.garth@csuglobal.edu>
2025-07-08 10:36:58 -0700

    Final draft with comments

--- Part1.py
+++ Part1.py
@@ -1,3 +1,5 @@
+# Method used to collect the amount of years from the user
+# Holds the user in a loop until input is valid.
 def get_years():
     user_input = 0
     while True:
@@ -7,16 +9,19 @@ def get_years():
             if user_input <= 0:
                 raise ValueError
             break
-        except ValueError:
+        except ValueError: # Exception raised if inputted data is invalid
             print('Please enter a value greater than zero.')
 
-    return user_input
+    return user_input # Returns integer
 
+# Method that takes an integer (number of years) and collects monthly
+# rainfall data using nested loops and stores it in a list and returns that list
 def gather_rainfall(years):
-    rainfall_data = []
+    rainfall_data = [] # Temporary collection
+    # Collection of month names for use in inner loop
     months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    for i in range(years):
-        for j in range(12):
+    for i in range(years): # Outer loop
+        for j in range(12): # Inner loop
             while True:
                 try:
                     temp = int(input('How much rain (in inches) occured for year {} in {}: '.format(i + 1, months[j])))
@@ -26,14 +31,17 @@ def gather_rainfall(years):
 
                     rainfall_data.append(temp)
                     break
-                except ValueError:
+                except ValueError: # Exception raised if input is invalid
                     print('Please enter a number 0 or greater')
-    return rainfall_data
+    return rainfall_data # Returns list
 
 def main():
-    years_requested = get_years()
-    monthly_rainfall = gather_rainfall(years_requested)
+    years_requested = get_years() # Calls method to collect number of years from user
+    
+    # Calls method inserting the number of years to receive list of collected rainfall
+    monthly_rainfall = gather_rainfall(years_requested) 
 
+    # Outputs report to screen
     print('-Rainfall Report-')
     print('  Number of Months: {}'.format(years_requested * 12))
     print('  Total Inches of rainfall: {}'.format(sum(monthly_rainfall)))
